4807c8e4ac508072a304e7dabc5d97c0e3388d36
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-12 21:57:19 +0200

    auto-updater initial version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,11 +3,10 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$toolsDir\commonEnv.ps1"
 
-$url        = 'https://github.com/zadam/trilium/releases/download/v0.42.3/trilium-windows-x64-0.42.3.zip'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = $url
+  url           = 'https://github.com/zadam/trilium/releases/download/v0.42.3/trilium-windows-x64-0.42.3.zip'
   checksum      = '67497B40B79819B1AA2795291BAAA4D544F2344C5CE5827F6E138ADE1F21B3D3'
   checksumType  = 'sha256'
 }

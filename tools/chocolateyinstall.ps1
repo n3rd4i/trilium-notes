cb9f4858f8fedd8a0b44722c2aefc647ff2c5cec
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-04-25 11:22:56 +0200

    updated to version 0.46.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/zadam/trilium/releases/download/v0.46.7/trilium-windows-x64-0.46.7.zip'
-  checksum      = '8bec4a885873624e1a6047a29f7cfff7e35ca02fccbde17966ada1dfcecfab64'
+  url           = 'https://github.com/zadam/trilium/releases/download/v0.46.9/trilium-windows-x64-0.46.9.zip'
+  checksum      = '975ce5ee0106ff9fc25c330f59b6778a135105615a36a99885d662e46c412246'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
